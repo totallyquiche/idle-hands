b20fe288d2f6cfb92085c93839c2a86c45321e74
Brian Dady <bndady@gmail.com>
2022-11-12 12:44:55 -0600

    Updates Timer to call tick handler and calculate idle time

--- idle-hands/Timer.js
+++ idle-hands/Timer.js
@@ -1,17 +1,28 @@
-class Timer {
+import PropertyManager from "./PropertyManager.js";
 
-  storage;
+class Timer extends PropertyManager {
 
-  constructor(storage) {
+  constructor(storage, tick = () => {}) {
+    super();
+
     if (!storage) throw new TypeError('storage must be defined');
 
-    this.storage = storage;
+    this.set('storage', storage);
+    this.set('startTime', this.getCurrentTime());
 
-    this.storage.set('startTime', this.getCurrentTime());
+    this.get('storage').set('startTime', this.get('startTime'));
+
+    setInterval(tick, 1000);
   }
 
   getCurrentTime() {
     return (new Date).getTime();
+  }
+
+  getIdleTime() {
+    const REAL_IDLE_TIME = this.getCurrentTime() - this.get('startTime');
+
+    return REAL_IDLE_TIME - (REAL_IDLE_TIME % 1000);
   }
 
 }
