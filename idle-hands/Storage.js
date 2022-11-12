19d3ba5bcf697d383ef932990a716ae33e54b9bf
Brian Dady <bndady@gmail.com>
2022-11-12 11:43:10 -0600

    Creats Storage class

---
+++ idle-hands/Storage.js
@@ -0,0 +1,25 @@
+class Storage {
+
+  prefix;
+
+  constructor(applicationId) {
+    if (!applicationId) throw new TypeError('applicationId must be defined');
+
+    this.prefix = applicationId + '_';
+  }
+
+  set(key, value) {
+    localStorage.setItem(this.prefix + key, value);
+  }
+
+  get(key) {
+    localStorage.getItem(this.prefix + key);
+  }
+
+  delete(key) {
+    localStorage.removeItem(this.prefix + key);
+  }
+
+};
+
+export default Storage;
