2a1aaaa09f4664281684a0efe98059555457b0c4
Brian Dady <bndady@gmail.com>
2022-11-13 20:30:37 -0600

    Sets logoutUrl before setting manualLogoutUrl

--- idle-hands/ConfigManager.js
+++ idle-hands/ConfigManager.js
@@ -1,33 +1,34 @@
 class ConfigManager {
 
   constructor(configValues) {
-    this.debug = false;
-    this.applicationId = window.location.hostname;
-    this.maximumIdleDuration = 60 * 1000 * 60; // 1 hoor;
-    this.heartbeatUrl = window.location.href;
-    this.heartbeatInterval = 60 * 1000; // 1 minute;
-    this.documentTitle = 'Session Expiration Warning';
-    this.logoutDocumentTitle = 'Logging out...';
-    this.promptDuration = 30 * 1000; // 30 seconds;
-    this.promptContainerSelector = 'body';
-    this.promptZindex = 9999;
-    this.promptDialogTextAllowHtml = false;
-    this.promptTimeRemainingTemplate = '%time seconds remaining';
-    this.promptHeaderText = 'Session Expiration Warning';
-    this.promptDialogText = 'Your session is about to expire due to inactivity.';
-    this.promptCancelButtonText = 'Stay Logged In';
-    this.promptLogoutButtonText = 'Log Out Now';
-    this.promptLogoutText = 'Logging out...';
-    this.events =  ['click', 'keypress', 'scroll', 'wheel', 'mousewheel'];
-    this.manualLogoutUrl = this.logoutUrl;
-
     if (!configValues['logoutUrl']) {
       throw new TypeError('logoutUrl must be defined');
     }
 
+    this.applicationId = window.location.hostname;
+    this.debug = false;
+    this.documentTitle = 'Session Expiration Warning';
+    this.events =  ['click', 'keypress', 'scroll', 'wheel', 'mousewheel'];
+    this.heartbeatInterval = 60 * 1000; // 1 minute;
+    this.heartbeatUrl = window.location.href;
+    this.logoutDocumentTitle = 'Logging out...';
+    this.maximumIdleDuration = 60 * 1000 * 60; // 1 hoor;
+    this.promptCancelButtonText = 'Stay Logged In';
+    this.promptContainerSelector = 'body';
+    this.promptDialogText = 'Your session is about to expire due to inactivity.';
+    this.promptDialogTextAllowHtml = false;
+    this.promptDuration = 30 * 1000; // 30 seconds;
+    this.promptHeaderText = 'Session Expiration Warning';
+    this.promptLogoutButtonText = 'Log Out Now';
+    this.promptLogoutText = 'Logging out...';
+    this.promptTimeRemainingTemplate = '%time seconds remaining';
+    this.promptZindex = 9999;
+
     for (const key in configValues) {
       this[key] = configValues[key];
     }
+
+    this.manualLogoutUrl = this.logoutUrl;
   }
 
 }
